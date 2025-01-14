fa29bda27f2854791dcaa78fbc01661bc320d199
Vignesh N <vigneshrage@gmail.com>
2025-01-14 13:29:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="bg-black h-screen">
+      <main className="max-w-7xl mx-auto ">
+        <Navbar />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <header className="fixed top-0 left-0 right-0 text-white z-50 bg-black/90">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between items-center py-4 mx-auto c-space">
+                    <a
+                        href="/"
+                        className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+                    >
+                        Vignesh
+                    </a>
+                    <button>
+                        <img src="" alt="" />
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
