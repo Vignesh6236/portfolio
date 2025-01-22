ad6b12c7f93e15d0c477460039562a463b3b71f4
Vignesh N <vigneshrage@gmail.com>
2025-01-22 15:14:49 +0530

    hero section, adding 3d model, positioning on website, scaling and totation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
-    <div className="bg-black h-screen">
+    <div className="bg-black text-white">
       <main className="max-w-7xl mx-auto ">
         <Navbar />
+        <Hero />
       </main>
     </div>
   );
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,76 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import React, { Suspense } from "react";
+import HackerRoom from "../components/HackerRoom";
+import { Canvas } from "@react-three/fiber";
+import CanvasLoader from "../components/CanvasLoader";
+import { Leva, useControls } from "leva";
+
+const Hero = () => {
+    const x = useControls("HackerRoom", {
+        positionX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        positionY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        positionZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        scale: {
+            value: 0.11,
+            min: 0.1,
+            max: 10,
+        },
+    });
+
+    return (
+        <section className="min-h-screen  w-full flex flex-col relative ">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 ">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+                    Hi, I am Vignesh <span className="waving-hand">👋</span>
+                </p>
+                <p className="hero_tag text-gray_gradient">
+                    Building Full Stack Applications and Websites
+                </p>
+            </div>
+            <div className="w-full h-full absolute inset-0">
+                <Leva />
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+                        <HackerRoom
+                            position={[0.8, -9.3, 2]}
+                            rotation={[0, -Math.PI, 0]}
+                            scale={0.1}
+                        />
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
